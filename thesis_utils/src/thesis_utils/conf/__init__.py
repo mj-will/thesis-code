6a02b1b7c2f8f9965dd6b076a3451e2b9ed0e34a
Michael Williams <michaeljw1@googlemail.com>
2022-10-22 14:52:28 +0100

    utils: add conf submodule

---
+++ thesis_utils/src/thesis_utils/conf/__init__.py
@@ -0,0 +1,9 @@
+"""Global configuration."""
+
+default_seed = 427
+"""The default random seed"""
+seed = None
+"""The current random seed"""
+
+figure_ftype = "pdf"
+"""File type for saving figures."""
