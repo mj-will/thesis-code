280186410bc6df995e4c745123fc58d64583e6bf
Michael Williams <michaeljw1@googlemail.com>
2022-10-22 14:51:00 +0100

    utils: add plotting utils

---
+++ thesis_utils/src/thesis_utils/plotting.py
@@ -0,0 +1,40 @@
+"""Utilities for plotting."""
+import importlib.resources
+import os
+from typing import Tuple
+
+import matplotlib
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from . import conf
+
+
+def set_plotting() -> None:
+    """Set the plotting defaults"""
+    sns.set_palette("colorblind")
+    # sns.set_style("ticks")
+    # sns.set_context("paper")
+    with importlib.resources.path("thesis_utils.conf", "thesis.mplstyle") as p:
+        plt.style.use(p)
+
+
+def get_default_figsize() -> Tuple[float, float]:
+    """Get the default figure size"""
+    return plt.rcParams["figure.figsize"]
+
+
+def save_figure(figure: matplotlib.figure.Figure, name: str, path: str) -> None:
+    """Save a figure with correct format.
+
+    Parameters
+    ----------
+    figure
+        The figure to save
+    name
+        Name for the file without the filetype
+    path
+        Path to the directory where the figure should be saved
+    """
+    filename = os.path.join(path, name + f".{conf.figure_ftype}")
+    figure.savefig(filename)
