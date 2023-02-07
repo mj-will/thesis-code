6eec1e7c13ed8ebab885cdd33a3f0cf3037e8c40
Michael Williams <michaeljw1@googlemail.com>
2023-02-07 10:53:45 +0000

    utils: add function to write JSON files

--- thesis_utils/src/thesis_utils/io.py
+++ thesis_utils/src/thesis_utils/io.py
@@ -7,3 +7,9 @@ def load_json(filename: str) -> dict:
     with open(filename, "r") as fp:
         d = json.load(fp)
     return d
+
+
+def write_json(data: dict, filename: str) -> None:
+    """Write data to a JSON file"""
+    with open(filename, "w") as fp:
+        json.dump(data, fp, indent=4)
