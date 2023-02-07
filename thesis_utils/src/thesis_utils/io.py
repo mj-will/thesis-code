a6de48c3b4f0615a6468e927670fd3d0464dea5d
mj-will <m.williams.4@research.gla.ac.uk>
2023-02-07 11:54:09 +0000

    utils: add load pickle

--- thesis_utils/src/thesis_utils/io.py
+++ thesis_utils/src/thesis_utils/io.py
@@ -1,5 +1,7 @@
 """I/O utilities"""
+from typing import Any
 import json
+import pickle
 
 
 def load_json(filename: str) -> dict:
@@ -13,3 +15,10 @@ def write_json(data: dict, filename: str) -> None:
     """Write data to a JSON file"""
     with open(filename, "w") as fp:
         json.dump(data, fp, indent=4)
+
+
+def load_pickle(filename: str) -> Any:
+    """Load a pickle file"""
+    with open(filename, "rb") as fp:
+        obj = pickle.load(fp)
+    return obj
