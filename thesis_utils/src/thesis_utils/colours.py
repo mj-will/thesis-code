d3f855653e114f353db584803cad129d6b43f8eb
mj-will <m.williams.4@research.gla.ac.uk>
2023-02-28 15:31:42 +0000

    utils: add colours

---
+++ thesis_utils/src/thesis_utils/colours.py
@@ -0,0 +1,12 @@
+"""Colours for use in my thesis."""
+
+teal = "#02979d"
+coral_pink = "#f97171"
+
+# UofG colours
+burgundy = "#7D2239"
+cobalt = "#005C8A"
+lavender = "#5B4D94"
+pillarbox = "#B30C00"
+rust = "#9A3A06"
+thistle = "#951272"
