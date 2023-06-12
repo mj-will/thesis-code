797d528c25bb55d32d990072d5e4d4d44cad94c7
mj-will <m.williams.4@research.gla.ac.uk>
2023-06-12 17:34:35 +0100

    nf: update coupling plot

--- normalising_flows/nf_coupling_plot.py
+++ normalising_flows/nf_coupling_plot.py
@@ -1,8 +1,10 @@
 """Produce a figure to show the stages of a coupling transform-based flow."""
 import os
+from typing import List, Optional
 
 import matplotlib
 import matplotlib.pyplot as plt
+from matplotlib import patches
 from nessai.flowmodel import FlowModel
 from nessai.livepoint import live_points_to_array
 from nessai_models import Rosenbrock
@@ -17,10 +19,13 @@ from thesis_utils.plotting import (
 )
 from thesis_utils.random import seed_everything
 from thesis_utils.sampling.rejection import rejection_sample_model
+import thesis_utils.colours as thesis_colours
 
 
 def plot_transforms(
-    x: np.ndarray, flow: FlowModel
+    x: np.ndarray,
+    flow: FlowModel,
+    arrow_colours: Optional[List[str]] = None,
 ) -> matplotlib.figure.Figure:
     """
     Plot the transformation of the samples after each transform in the flow
@@ -80,6 +85,29 @@ def plot_transforms(
                 ax[i].scatter(o[j, 0], o[j, 1], color=c, s=1.0)
             # ax[i].set_title(f"Transform {i}")
 
+        if arrow_colours is not None:
+            for i in range(n):
+
+                arrowlen = 0.125
+
+                xyA = [1 - arrowlen, 0.5]
+                # ax[i].plot(*xyA, "o")
+                xyB = [0 + arrowlen, 0.5]
+                # ax[i + 1].plot(*xyB, "o")
+                # ConnectionPatch handles the transform internally so no need to get fig.transFigure
+                arrow = patches.ConnectionPatch(
+                    xyA,
+                    xyB,
+                    coordsA=ax[i].transAxes,
+                    coordsB=ax[i + 1].transAxes,
+                    # Default shrink parameter is 0 so can be omitted
+                    color=arrow_colours[i],
+                    arrowstyle="-|>",  # "normal" arrow
+                    mutation_scale=10,  # controls arrow head size
+                    linewidth=2.0,
+                )
+                fig.patches.append(arrow)
+
         ax[0].set_title("Input")
         ax[-1].set_title("Output")
         plt.tight_layout()
@@ -112,6 +140,8 @@ def main() -> None:
     )
 
     flow = FlowModel(output=outdir, config=config)
+    flow.initialise()
+    print(flow.model)
 
     data_unstruct = live_points_to_array(data, names=model.names)
 
@@ -121,7 +151,16 @@ def main() -> None:
 
     flow.train(data_unstruct)
 
-    fig = plot_transforms(data_unstruct, flow.model)
+    fig = plot_transforms(
+        data_unstruct[:2000],
+        flow.model,
+        arrow_colours=[
+            thesis_colours.pillarbox,
+            thesis_colours.yellow,
+            thesis_colours.pillarbox,
+            thesis_colours.yellow,
+        ],
+    )
 
     save_figure(fig, "coupling_breakdown", "figures")
 
